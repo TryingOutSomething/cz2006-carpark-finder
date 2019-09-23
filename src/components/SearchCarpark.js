05597c6c79dca82f18aa56effbacf0d5da3d121e
TryingOutSomething <43298887+TryingOutSomething@users.noreply.github.com>
2019-09-23 20:57:33 +0800

    Added touch inout to list

--- src/components/SearchCarpark.js
+++ src/components/SearchCarpark.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import {
-  Button,
   FlatList,
   StyleSheet,
+  ScrollView,
   Text,
   TextInput,
-  ScrollView,
+  TouchableOpacity,
   View
 } from "react-native";
+
+import { Button } from "react-native-paper";
 
 import SearchCarpark from "../controllers/CarParkDataHandler";
 
@@ -25,28 +27,35 @@ export default class FindCarparks extends Component {
 
     this.getSearchCarpark = SearchCarpark.bind(this);
     this.renderCarParkList = this.renderCarParkList.bind(this);
+    this.log = this.log.bind(this);
   }
 
   renderCarParkList(carpark_address) {
+    if (!carpark_address) return;
     this.setState({ isFetching: true });
 
-    this.getSearchCarpark(carpark_address).then(value =>
-      this.setState({ carParkInfo: value })
-    );
+    this.getSearchCarpark(carpark_address).then(value => {
+      this.setState({ carParkInfo: value });
+    });
+  }
+
+  log(item) {
+    console.log(item);
   }
 
   render() {
     return (
       <View style={styles.container}>
         <TextInput
-          style={styles.textarea}
+          style={styles.TextArea}
           onChangeText={text => this.setState({ textInput: text })}
           value={this.state.textInput}
           placeholder="Enter Carpark"
         />
         <Button
           onPress={() => this.renderCarParkList(this.state.textInput)}
-          title="Search"
+          mode="contained"
+          children="Search"
         />
 
         {this.state.isFetching ? (
@@ -54,7 +63,18 @@ export default class FindCarparks extends Component {
             <ScrollView>
               <FlatList
                 data={this.state.carParkInfo}
-                renderItem={({ item }) => <Text>{item.address}</Text>}
+                renderItem={({ item }) => (
+                  <View>
+                    <TouchableOpacity
+                      style={styles.ListItem}
+                      onPress={() => {
+                        this.log(item);
+                      }}
+                    >
+                      <Text>{item.address}</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
                 keyExtractor={item => item._id.toString()}
                 ListEmptyComponent={<Text>No Results!</Text>}
               />
@@ -72,9 +92,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  textarea: {
+  TextArea: {
     height: 40,
     borderColor: "gray",
     borderWidth: 1
+  },
+  ListItem: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16
   }
 });
